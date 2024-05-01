1ef78bd0da0fb5f27a0aa73ca0a80464a1a39b49
David Flores <davidflores7_8@hotmail.com>
2024-05-01 22:56:49 +0000

    Adding some fixes

--- obs_stack/ch12-completed/event-automation.py
+++ obs_stack/ch12-completed/event-automation.py
@@ -1,33 +1,5 @@
-import os
-from typing import Literal
-
 import requests
 from prefect import flow, task
-from prefect.blocks.system import Secret
-from pydantic import BaseModel
-
-
-class AlertmanagerAlert(BaseModel):
-    status: str
-    labels: dict
-    annotations: dict
-    startsAt: str
-    endsAt: str
-    generatorURL: str
-    fingerprint: str
-
-
-class AlertmanagerAlertGroup(BaseModel):
-    version: str
-    groupKey: str
-    truncatedAlerts: int
-    status: Literal["firing", "resolved"]
-    receiver: str
-    groupLabels: dict
-    commonLabels: dict
-    commonAnnotations: dict
-    externalURL: str
-    alerts: list[AlertmanagerAlert]
 
 
 def get_nautobot_secret() -> str:
@@ -37,7 +9,7 @@ def get_nautobot_secret() -> str:
 
 
 @task(task_run_name="[RETRIEVE] {device}", retries=3, log_prints=True)
-def retrieve_device_info(device: str) -> dict:
+def get_nautobot_intf_id(device: str, interface: str) -> str | None:
     """Retrieve device information from Nautobot.
 
     Args:
@@ -57,113 +29,60 @@ def retrieve_device_info(device: str) -> dict:
         }
     }
     """
-    print("Retrieving device information")
     response = requests.post(
         url="http://localhost:8080/api/graphql/",
         headers={"Authorization": f"Token {get_nautobot_secret()}"},
         json={"query": gql, "variables": {"device": device}},
     )
     response.raise_for_status()
-    return response.json()["data"]["devices"][0]
-
-
-@task(task_run_name="[UPDATE] {device_info[name]}: {interface} status {status}", log_prints=True)
-def update_device_intf_status(device_info: dict, interface: str, status: Literal["firing", "resolved"]) -> bool:
-    """Update the device interface status.
-
-    Example device_info:
-
-    {
-        "name": "ceos-01",
-        "interfaces": [
-          {
-            "name": "Ethernet1",
-            "id": "d3f6335f-1d6c-429f-9c5c-c62f5d6c1363"
-          },
-          {
-            "name": "Ethernet2",
-            "id": "5821eda8-affb-4209-bcd8-f39fc6876cc3"
-          },
-          {
-            "name": "Loopback0",
-            "id": "79b617b1-d9b7-4ad1-855f-15e02e702b5f"
-          },
-          {
-            "name": "Loopback1",
-            "id": "7f81041e-db6f-4002-8315-0ada1467a7b6"
-          },
-          {
-            "name": "Management0",
-            "id": "0d513d72-7089-4c7b-b484-8ae5a18caf91"
-          }
-        ]
-      }
-
-    Args:
-        device_info (dict): The device information.
-        interface (str): The interface name.
-        status (Literal["firing", "resolved"]): The status to update.
-
-    Returns:
-        bool: True if the status was updated successfully.
-    """
-    # Search for the interface
-    interface_id = None
-    for intf in device_info["interfaces"]:
+    result = response.json()["data"]["devices"][0]
+    for intf in result["interfaces"]:
         if intf["name"] == interface:
-            interface_id = intf["id"]
-            break
+            return intf["id"]
 
-    if interface_id is None:
-        print(f"Interface {interface} not found on device {device_info['name']}")
-        return False
 
-    # Update the interface status
-    try:
-        result = requests.patch(
-            url=f"http://localhost:8080/api/dcim/interfaces/{interface_id}/",
-            headers={"Authorization": f"Token {get_nautobot_secret()}"},
-            json={"status": "lab-active"} if status == "resolved" else {"status": "Alerted"},
-        )
-        result.raise_for_status()
-    except Exception as e:
-        print(f"Failed to update interface {interface} status on device {device_info['name']}: {e}")
-        return False
+@flow(flow_run_name="Update Nautobot {device}:{interface} - status {status}", log_prints=True)
+def update_nautobot_intf_state(device: str, interface: str, status: str) -> bool:
+    """Update Nautobot with the latest information."""
+    # Retrieve Nautobot Interface ID
+    intf_id = get_nautobot_intf_id(device=device, interface=interface)
 
-    print(f"Interface {interface} status updated to {status} on device {device_info['name']}")
+    # Print the interface ID
+    print(f"Interface: {interface} == ID: {intf_id}")
 
-    return True
-
+    # Update the device interface status in Nautobot
+    is_good = update_device_intf_status(intf_id=intf_id, status=status)
+    return is_good
 
 @flow(log_prints=True)
-def alert_receiver(alert_group: AlertmanagerAlertGroup):
+def alert_receiver(alert_group: dict):
     """Process the alert."""
-    print(f"Received alertmanager alert group with status {alert_group.status} -- {alert_group.groupLabels}")
-    # Check te subject of the alert and forward to respective workflow
-    if alert_group.groupLabels["alertname"] == "PeerInterfaceFlapping":
-        for alert in alert_group.alerts:
+    # Status of the alert group
+    status = alert_group["status"]
+
+    # Name of the alert group
+    alertgroup_name = alert_group["groupLabels"]["alertname"]
+
+    # Alerts in the alert group
+    alerts = alert_group["alerts"]
+
+    print(f"Received alert group: {alertgroup_name} - {status}")
+
+    # Check the subject of the alert and forward to respective workflow
+    if alertgroup_name == "PeerInterfaceFlapping":
+        for alert in alerts:
 
             # Run the peer interface flap workflow
-            result = peer_interface_flap(
-                device=alert.labels["device"],
-                interface=alert.labels["interface"],
-                status=alert_group.status,
+            result = update_nautobot_intf_state(
+                device=alert["labels"]["device"],
+                interface=alert["labels"]["interface"],
+                status=alert_group["status"],
             )
 
             # Print the result to console
             print(f"Peer Interface Flap Workflow Successful: {result}")
+
     print("Alertmanager Alert Group status processed, exiting")
-
-
-@flow(flow_run_name="Peer Interface Flap Workflow {device}:{interface} - status {status}", log_prints=True)
-def peer_interface_flap(device: str, interface: str, status: Literal["firing", "resolved"]) -> bool:
-    """Update Nautobot with the latest information."""
-    # Retrieve the device information
-    device_info = retrieve_device_info(device=device)
-    # Print the device information to console for debug purposes
-    print(device_info)
-    # Update the device status
-    return update_device_intf_status(device_info=device_info, interface=interface, status=status)
 
 
 if __name__ == "__main__":
